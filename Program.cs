fd0ee646a2efb72b5593b07daa926599c3028140
25081997pc <chavanpooja2508@gmail.com>
2022-06-06 20:31:24 +0530

    Using queueEnqueue

--- Program.cs
+++ Program.cs
@@ -7,15 +7,21 @@ namespace DataStructureProblems
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Solving Problems on Stack and Queue");
-           
-            LinkedListStack obj = new LinkedListStack();
-            obj.Push(70);
-            obj.Push(30);
-            obj.Push(56);
 
-            obj.Display();
-            obj.IsEmpty();
-            
+            //LinkedListStack obj = new LinkedListStack();
+            //obj.Push(70);
+            //obj.Push(30);
+            //obj.Push(56);
+
+            //obj.Display();
+            //obj.IsEmpty();
+
+            LinkedListQueue queue = new LinkedListQueue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+
+            queue.Display();
         }
     }
 }
